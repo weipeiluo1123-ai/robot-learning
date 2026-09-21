f619419d751ee86aa7e03f60a51b34ee8818aaf4
WeiPei Luo <weipeiluo1123@outlook.com>
2026-09-21 10:18:54 +0800

    docs(mujoco): add comments to two-link arm script

--- mujoco/03_two_link_arm/main.py
+++ mujoco/03_two_link_arm/main.py
@@ -33,18 +33,20 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
         step_start = time.perf_counter()
 
+        # 决定控制命令 控制信号在前 0.5 秒为 [0.6, -0.8]，之后为 [-0.6, 0.8]
         if data.time < 0.5:
             data.ctrl[:] = [0.6, -0.8]
         else:
-            data.ctrl[:] = [-0.6, 2.5]
+            data.ctrl[:] = [-0.6, 0.8]
 
         mujoco.mj_step(model, data)
 
+        # 获取末端执行器位置并记录轨迹 这里的"end_effector"是xml中定义的site名称 .xpos是末端执行器在世界坐标系下的位置如(x,y,z)
         end_effector_pos = data.site("end_effector").xpos.copy()
         trail.append(end_effector_pos)
 
         if len(trail) > 200:
-            trail.pop(0)
+            trail.pop(0)    # 弹出最老的那一个
 
         print(
             f"time={data.time:.2f}, "
@@ -55,12 +57,16 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
             f"target2={data.ctrl[1]:.3f}"
         )
 
+        # 开始画轨迹
         viewer.user_scn.ngeom = 0
 
+        # enumerate(trail)返回一个迭代器，返回值是一个元组，元组的第一个元素是索引，第二个元素是trail中的元素
         for i, point in enumerate(trail):
+            # user_scn 能存的 visualization geometry 数量不是无限的
             if i >= viewer.user_scn.maxgeom:
                 break
 
+            # 真正在points里创建绿色小球
             mujoco.mjv_initGeom(
                 viewer.user_scn.geoms[i],
                 type=mujoco.mjtGeom.mjGEOM_SPHERE,
@@ -70,11 +76,13 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
                 rgba=[0.2, 1.0, 0.3, 0.8],
             )
 
+        # 告诉 Viewer 到底画几个
         viewer.user_scn.ngeom = min(
             len(trail),
             viewer.user_scn.maxgeom,
         )
 
+        # 更新viewer
         viewer.sync()
 
         target_wall_step = model.opt.timestep / playback_speed
